5794165f09d0e6715fd0713a3121258871590674
charan-singh-thakur <157889476+charan-singh-thakur@users.noreply.github.com>
2026-04-18 01:09:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
-from telethon import TelegramClient, events
 import re
 import requests
 
 print("BOT STARTED")
 
-api_id = 31617683
-api_hash = "1abcdb1e9787e95d194052dc56eada05"
 bot_token = "8741386792:AAGpqxQdeuCf66AIAu74PgXW5J2IENlbwhw"
 
 source_channel = -1002071984432
 target_channel = -1002113648943
-
-client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 keywords = [
     "sridevi",
@@ -23,19 +18,41 @@ keywords = [
     "main"
 ]
 
-@client.on(events.NewMessage(chats=source_channel))
-async def handler(event):
-    text = event.message.text.lower()
-    print("MESSAGE:", text)
+last_message_id = None
 
-    if any(word in text for word in keywords):
+def get_updates():
+    global last_message_id
 
-        match = re.search(r'\d{3}-\d{2}-\d{3}|\d{3}-\d', text)
+    url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
+    response = requests.get(url).json()
 
-        if match:
-            result = match.group()
+    for update in response.get("result", []):
+        message = update.get("channel_post")
 
-            msg = f"""🔻FASTEST LIVE UPDATE🔻
+        if message:
+            msg_id = message["message_id"]
+
+            if last_message_id == msg_id:
+                continue
+
+            last_message_id = msg_id
+
+            chat_id = message["chat"]["id"]
+
+            if chat_id != source_channel:
+                continue
+
+            text = message.get("text", "").lower()
+            print("MESSAGE:", text)
+
+            if any(word in text for word in keywords):
+
+                match = re.search(r'\d{3}-\d{2}-\d{3}', text)
+
+                if match:
+                    result = match.group()
+
+                    msg = f"""🔻FASTEST LIVE UPDATE🔻
 
 SRIDEVI NIGHT
 
@@ -51,9 +68,13 @@ LIVE UPDATES FOLLOW:=👇
 ➖➖➖➖➖➖➖➖➖
 """
 
-            requests.post(
-                f"https://api.telegram.org/bot{bot_token}/sendMessage",
-                data={"chat_id": target_channel, "text": msg}
-            )
+                    requests.post(
+                        f"https://api.telegram.org/bot{bot_token}/sendMessage",
+                        data={"chat_id": target_channel, "text": msg}
+                    )
 
-client.run_until_disconnected()
+while True:
+    try:
+        get_updates()
+    except Exception as e:
+        print("ERROR:", e)
